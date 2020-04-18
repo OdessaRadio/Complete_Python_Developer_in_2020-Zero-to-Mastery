ca53c5258db6d85d364af4b6e2e144f616e3c4c8
OdessaRadio <alexander_lyashko@hotmail.com>
2020-04-18 22:47:51 +0000

    Technical commit

--- Our_First_Python_Program_2.8/heyhey.py
+++ Our_First_Python_Program_2.8/heyhey.py
@@ -1,2 +1,6 @@
 # First program from the course
-print ("Hi")
+print ("This script will calculate sum of 2 integers")
+num1 = input("Enter number 1\n")
+num2 = input("Enter number 2\n")
+num3 = int(num1) + int(num2)
+print("Result: "+str(num3))
