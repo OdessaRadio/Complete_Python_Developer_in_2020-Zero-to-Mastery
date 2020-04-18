31c1b0227f5f5ba5f2610899dcec0c531d4b62ba
OdessaRadio <alexander_lyashko@hotmail.com>
2020-04-18 22:49:58 +0000

    Added 3 number

--- Our_First_Python_Program_2.8/heyhey.py
+++ Our_First_Python_Program_2.8/heyhey.py
@@ -1,6 +1,7 @@
 # First program from the course
-print ("This script will calculate sum of 2 integers")
+print ("This script will calculate sum of 3 integers")
 num1 = input("Enter number 1\n")
 num2 = input("Enter number 2\n")
-num3 = int(num1) + int(num2)
-print("Result: "+str(num3))
+num3 = input("Enter number 2\n")
+num4 = int(num1) + int(num2) + int(num3) 
+print("Result: "+str(num4))
