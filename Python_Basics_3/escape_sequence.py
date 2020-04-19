e7bb08d70dc44417297b5351990738af9f096155
OdessaRadio <alexander_lyashko@hotmail.com>
2020-04-19 15:16:16 +0000

    escape sequencies

---
+++ Python_Basics_3/escape_sequence.py
@@ -0,0 +1,11 @@
+# Escape sequence \ we using slash to tellPython that everthing that comes up after \ is a string
+
+weather1 = 'it\'s \'kind of sunny\' sunny'
+print(weather1)
+
+# \t adding a tab
+weather2 = '\tit\'s a good weather'
+print(weather2)
+
+weather3 = '\tit\'s a good weather. \nLet\'s go walk'
+print(weather3)
