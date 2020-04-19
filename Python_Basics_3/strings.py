bc780c7d0bc8ccccc9a353e948b2de1146a598c6
OdessaRadio <alexander_lyashko@hotmail.com>
2020-04-19 15:00:54 +0000

    Strings

---
+++ Python_Basics_3/strings.py
@@ -0,0 +1,24 @@
+print(type("hi hello there"))
+user_name = "super coder"
+user_password = "super password"
+
+long_string='''
+WOW
+Really imprssed
+0000
+----
+''' 
+
+print(long_string)
+
+first_name = "Oleksandr"
+last_name = "Liashko"
+full_name = first_name +" "+ last_name
+
+print(full_name)
+
+print ("==============")
+
+# String concatenation
+
+print("Hello" + " " + "Sasha")
