7ddb351241f39bc44a108cd47e08ad2276bdd68c
OdessaRadio <alexander_lyashko@hotmail.com>
2020-04-19 19:57:33 +0000

    exercise type conversion

---
+++ Python_Basics_3/exercise_type_conversion.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+name = "Sasha"
+age = 50
+relationship_status = "Single"
+
+relationship_status = 'it\'s complicated'
+# print(relationship_status)
+
+birth_year = input("What year were you born?")
+year = str((datetime.now()))[:4]
+
+print(year)
+print(type(year), type(birth_year))
+
+age = int(year) - int(birth_year)
+print(f'You are {age} years old')
