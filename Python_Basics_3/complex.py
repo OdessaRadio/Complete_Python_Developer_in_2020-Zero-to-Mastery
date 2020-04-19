b4e7d4113c6ec3d155f4af697e05f9870c14d641
OdessaRadio <alexander_lyashko@hotmail.com>
2020-04-19 10:15:29 +0000

    bin() and complex

---
+++ Python_Basics_3/complex.py
@@ -0,0 +1,7 @@
+# py script is for bin() and complex tasks 3.23
+
+# complex -> complex numbers
+
+print(bin(5)) # binary representation 
+
+print(int("0b101", 2)) # convert number presented in bimary to integer, 2 is base
