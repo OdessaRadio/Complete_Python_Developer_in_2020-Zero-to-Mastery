52f8adab40d7c7e07e1e8f5ac3f33acf74ded941
OdessaRadio <alexander_lyashko@hotmail.com>
2020-04-19 14:38:30 +0000

    variables

---
+++ Python_Basics_3/variables.py
@@ -0,0 +1,21 @@
+PI=3.14 # Constant
+
+a,b,c = 1,2,"3"
+
+# __ 2 underscores is called "dunder"
+
+_user_iq = 190 #snake_case
+
+_user_IQ = 220
+
+_user_IQ1 = _user_IQ // 4
+
+# print(type(id))
+
+print (_user_iq)
+print (_user_IQ)
+print (_user_IQ1)
+print (PI)
+
+print (a,b,c)
+print(type(a),type(b),type(c))
