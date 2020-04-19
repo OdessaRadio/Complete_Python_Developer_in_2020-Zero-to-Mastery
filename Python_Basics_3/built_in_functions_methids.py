4647c1d1a7d6c7a9df55352933469fa069c42d4c
OdessaRadio <alexander_lyashko@hotmail.com>
2020-04-19 19:32:21 +0000

    built in functions and methods

---
+++ Python_Basics_3/built_in_functions_methids.py
@@ -0,0 +1,18 @@
+print(len('0123456789')) # len -> lenght ofthe string
+
+greet = '0123456789'
+print(greet[1:])
+print(greet[0:len(greet)])
+
+
+quote = "to be or not to be"
+print(quote.upper())
+
+print(quote.capitalize())
+
+print(quote.find('be'))
+
+print(quote.replace('be','me'))
+
+print(quote) # strings are immutable. So it will print "to be or not to be"
+
