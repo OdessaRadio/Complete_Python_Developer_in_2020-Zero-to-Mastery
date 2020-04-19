75f43cc22c0043450b7de90cbd9504096ff21cc3
OdessaRadio <alexander_lyashko@hotmail.com>
2020-04-19 07:09:52 +0000

    math functions

--- Python_Basics_3/main.py
+++ Python_Basics_3/main.py
@@ -5,7 +5,13 @@
 #print (type(2/4))
 
 
-print (type(10.1+2.9))
-print (10.1+2.9)
-print (type(9//4))
-print (8%4)
+# print (type(10.1+2.9))
+# print (10.1+2.9)
+# print (type(9//4))
+# print (8%4)
+
+print("=====")
+# math functions
+print (round(10.65))
+print (abs(-10.65))
+
