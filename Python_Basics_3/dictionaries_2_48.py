9554dfccd57992c697de82fe0b4b6b5a58ca58bd
OdessaRadio <alexander_lyashko@hotmail.com>
2020-04-26 19:14:37 +0000

    Dictionaries_2_48

---
+++ Python_Basics_3/dictionaries_2_48.py
@@ -0,0 +1,28 @@
+# Dictionary
+# 2_48
+
+dictionary = {
+  'a' : [1,2,3],
+  'b' : 'hello',
+  'x' : True
+}
+
+my_list=[
+  {
+  'a' : [1,2,3],
+  'b' : 'hello',
+  'x' : True
+},
+{
+  'a' : [4,5,6],
+  'b' : 'hello',
+  'x' : True
+}
+]
+
+print(my_list[0]['a'][2])
+print(dictionary['b'])
+
+# Understanding Data Structures
+# 2_49 
+
