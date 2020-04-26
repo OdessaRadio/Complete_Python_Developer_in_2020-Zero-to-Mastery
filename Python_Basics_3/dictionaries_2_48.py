1008b27f8eae674bf7831b17e85c9719c8fb3b58
OdessaRadio <alexander_lyashko@hotmail.com>
2020-04-26 19:18:19 +0000

    Dictionaries_2_49

--- Python_Basics_3/dictionaries_2_48.py
+++ Python_Basics_3/dictionaries_2_48.py
@@ -20,9 +20,21 @@ my_list=[
 }
 ]
 
-print(my_list[0]['a'][2])
-print(dictionary['b'])
+# print(my_list[0]['a'][2])
+# print(dictionary['b'])
 
 # Understanding Data Structures
 # 2_49 
+
+# Dictionary has no orde
+# List is ordered
+
+# Dictionary holds more information than a list
+# Liat is just an index and Value
+
+dict_10 ={
+  'weapons' : [1,2,3],
+  'greeting' : 'hello',
+  'is_Magic' : True
+}
 
