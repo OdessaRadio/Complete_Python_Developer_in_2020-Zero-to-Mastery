15ce07a6b4f97ca7788a85c1a69bf938cf9e649d
OdessaRadio <alexander_lyashko@hotmail.com>
2020-05-07 16:02:28 +0000

    Set 2 2_56

--- Python_Basics_3/sets_2_55.py
+++ Python_Basics_3/sets_2_55.py
@@ -4,19 +4,52 @@
 
 my_list = [1,2,3,4,5,5,6,6,7,7,8]
 
-my_set = {1,2,3,4,5,5,6,6,7,7,81,2,3,4,5,5} # set
+my_set = {1,2,3,4,5} # set
+your_set = {4,5,6,7,8,9,10}
 my_set.add(2)
 my_set.add(100)
 
-print(1 in my_set)
-print(len(my_set))
-
-print(list(my_set))
-
-print(set(my_list)) # Remove all duplicates from the list
+# print(1 in my_set)
+# print(len(my_set))
+# print(list(my_set))
+# print(set(my_list)) # Remove all duplicates from the list
 
 new_set = my_set.copy()
 
 my_set.clear()
-print(my_set)
+# print(my_set)
+
+my_set = {4,5} # set
+your_set = {4,5,6,7,8,9,10}
+
+# difference method
+# print(my_set.difference(your_set))
+# print(my_set)
+# print(your_set.difference(my_set))
+
+# discard method
+# print(my_set.discard(5))
+# print(my_set)
+
+# difference_update
+# print(my_set.difference_update(your_set))
+# print(my_set)
+
+# intersection
+#print(my_set.intersection(your_set))
+#print(my_set & your_set)
+
+
+# isdisjoint
+# print(my_set.isdisjoint(your_set))
+
+# union
+#print(my_set.union(your_set))
+#print(my_set | your_set)
+
+# issubset
+# print(my_set.issubset(your_set))
+
+# issuperset
+# print(my_set.issuperset(your_set))
 
