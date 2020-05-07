e18480b35c4582e5330189475db3e404f56880fa
OdessaRadio <alexander_lyashko@hotmail.com>
2020-05-07 15:33:58 +0000

    Tuples_2

--- Python_Basics_3/tuples_2_53.py
+++ Python_Basics_3/tuples_2_53.py
@@ -14,7 +14,30 @@ user = {
   (1,2,3,4) : ('a', 'b', 'c')
 }
 
-print(user.items())
+# print(user.items())
 
-print(user[(1,2,3,4)])
+# print(user[(1,2,3,4)])
+
+# Tuples 2  2_54
+
+my_tuple = (1,2,3,4,5,1,1,1,1)
+
+new_tuple = my_tuple[1:2]
+
+x = my_tuple[0]
+y = my_tuple[1]
+
+# print(x,y)
+
+# print(new_tuple)
+
+x,y,z, *other = (1,2,3,4,5,1,1,1)
+# print(other)
+
+print(my_tuple.count(5))
+
+print(my_tuple.index(1)) 
+
+print(len(my_tuple))
+
 
