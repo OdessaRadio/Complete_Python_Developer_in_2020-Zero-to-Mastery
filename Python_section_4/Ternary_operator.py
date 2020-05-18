de9712647e87303f1af7ef7ab77d4b1108ce5c89
OdessaRadio <alexander_lyashko@hotmail.com>
2020-05-18 11:42:35 +0000

    Ternary operator4_60

---
+++ Python_section_4/Ternary_operator.py
@@ -0,0 +1,10 @@
+# Ternary_operator 4_61
+# conditional expression
+
+# condition_if_true if condition else cindition_if_else
+
+is_friend = False
+can_message = "message allowed" if is_friend else "not allowed to message"
+
+print(can_message)
+
