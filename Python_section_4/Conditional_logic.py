a6ddb99f9c89e5d1f9fa3c5aa7cfefd33ce6b905
OdessaRadio <alexander_lyashko@hotmail.com>
2020-05-18 11:34:09 +0000

    4_59

--- Python_section_4/Conditional_logic.py
+++ Python_section_4/Conditional_logic.py
@@ -1,16 +1,33 @@
 # Conditional Logic 4_58
 
-is_old = True
-is_licenced = True
-is_good_driver = True
+# is_old = 'hello'
+# is_licenced = 5
+# is_good_driver = True
 
-if is_old and is_licenced:
-  print("You are old enough to drive! And you have a license")
-elif is_licenced:
-  print("You can drive now")
-elif is_good_driver:
-  print("Good driver")
-else:
-  print("checkcheck")
+# if is_old and is_licenced:
+#   print("You are old enough to drive! And you have a license")
+# elif is_licenced:
+#   print("You can drive now")
+# elif is_good_driver:
+#   print("Good driver")
+# else:
+#   print("checkcheck")
 
-print("okok")
+# print("okok")
+
+
+# Indentation In Python 4_59
+
+#tab = 4 spaces
+
+# Truthy and Falsy 4_60
+
+print(bool(None))
+print(bool(""))
+
+password = '123'
+username = 'johnny'
+
+if password and username:
+  print('pass and user exist')
+
