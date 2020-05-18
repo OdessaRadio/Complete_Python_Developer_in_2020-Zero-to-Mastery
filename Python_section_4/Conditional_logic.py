8df3692992d9497965e48b0f6399fb3a39122c80
OdessaRadio <alexander_lyashko@hotmail.com>
2020-05-18 11:20:47 +0000

    Conditional_logic_lesson_4_58_completed

--- Python_section_4/Conditional_logic.py
+++ Python_section_4/Conditional_logic.py
@@ -1,11 +1,11 @@
 # Conditional Logic 4_58
 
-is_old = False
-is_licenced = False
+is_old = True
+is_licenced = True
 is_good_driver = True
 
-if is_old:
-  print("You are old enough to drive!")
+if is_old and is_licenced:
+  print("You are old enough to drive! And you have a license")
 elif is_licenced:
   print("You can drive now")
 elif is_good_driver:
