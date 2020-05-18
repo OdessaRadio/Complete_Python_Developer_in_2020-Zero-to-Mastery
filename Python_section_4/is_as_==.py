3fafbe29edf98c034b41e274b9c3be8b0acc7de2
OdessaRadio <alexander_lyashko@hotmail.com>
2020-05-18 13:26:12 +0000

    is vs == is 4_65

--- Python_section_4/is_as_==.py
+++ Python_section_4/is_as_==.py
@@ -1,10 +1,18 @@
 # is as == 4_65
 
-print(True == 1)
-print('' == 1)
-print([] == 1)
-print(10 == 10.0)
-print([] == [])
+print(True is True)
+print('1' is '1')
+print([] is []) # False because adding a list is a new location in the memory
+print(10 is 10.0)
+print([1,2,3] is [1,2,3]) # data structure. Each time we create it, it is located in new memory area
 
-# == checks for equality for Value. Different data types will be converted to bool
+a = [1,2,3]
+b = [1,2,3]
+
+print(a == b)
+
+# == checks for equality of Values. Different data types will be converted to bool
+
+# is check is the location in the memory were the value is stored is the same
+
 
