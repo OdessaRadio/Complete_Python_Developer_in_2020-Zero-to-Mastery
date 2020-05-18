a3325361217d6e37183ae38419db6840e703601e
OdessaRadio <alexander_lyashko@hotmail.com>
2020-05-18 11:17:37 +0000

    Conditional_logic_lesson_4_58

---
+++ Python_section_4/Conditional_logic.py
@@ -0,0 +1,16 @@
+# Conditional Logic 4_58
+
+is_old = False
+is_licenced = False
+is_good_driver = True
+
+if is_old:
+  print("You are old enough to drive!")
+elif is_licenced:
+  print("You can drive now")
+elif is_good_driver:
+  print("Good driver")
+else:
+  print("checkcheck")
+
+print("okok")
