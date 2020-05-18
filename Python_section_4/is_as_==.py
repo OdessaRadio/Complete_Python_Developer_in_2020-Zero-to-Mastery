e00bd8147371a8fab225a4ff53d82802ed45cb79
OdessaRadio <alexander_lyashko@hotmail.com>
2020-05-18 13:21:42 +0000

    is vs == == 4_65

---
+++ Python_section_4/is_as_==.py
@@ -0,0 +1,10 @@
+# is as == 4_65
+
+print(True == 1)
+print('' == 1)
+print([] == 1)
+print(10 == 10.0)
+print([] == [])
+
+# == checks for equality for Value. Different data types will be converted to bool
+
